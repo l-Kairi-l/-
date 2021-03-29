16bcd705a81246cb98daafd8f0cbd028782c0ac8
菊池　海璃 <@LAPTOP-DDSV0RHM>
2021-03-29 23:49:31 +0900

    背景　モードの追加

---
+++ CutWist/Assets/Camera/CameraContolloer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContolloer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject PlayCamera;      //メインカメラ格納用
+    private GameObject EditCamera;       //サブカメラ格納用 
+
+    bool NowCamera;
+
+    void Start()
+    {
+        //プレイカメラとエディットカメラをそれぞれ取得
+        PlayCamera = GameObject.Find("PlayCamera");
+        EditCamera = GameObject.Find("EditCamera");
+        //エディットカメラを非アクティブにする
+        PlayCamera.SetActive(false);
+
+        NowCamera = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //スペースキーが押されている間、エディットカメラをアクティブにする
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            NowCamera = !NowCamera;
+            //サブカメラをアクティブに設定
+            PlayCamera.SetActive(NowCamera);
+            EditCamera.SetActive(!NowCamera);
+        }
+    }
+}
---
+++ CutWist/Assets/Stage1/1/Resources/CloudResources.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudResources : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Use this for initialization
+    int Count;
+
+    void Start()
+    {
+        Count = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //一番手前
+        if (Random.value <= 0.001f)
+        {
+            // CubeプレハブをGameObject型で取得
+            GameObject obj = (GameObject)Resources.Load("cloud");
+            // Cubeプレハブを元に、インスタンスを生成、
+            Instantiate(obj, new Vector3(-30.0f, Random.value * 20.0f + -10.0f, 70.0f), Quaternion.identity);
+            obj.GetComponent<Cloud>().SetCloud(0.03f, 1.0f, 1.5f);
+
+        }
+
+        //真ん中
+        if (Random.value <= 0.001f)
+        {
+            // CubeプレハブをGameObject型で取得
+            GameObject obj = (GameObject)Resources.Load("cloud");
+            // Cubeプレハブを元に、インスタンスを生成、
+            Instantiate(obj, new Vector3(-30.0f, Random.value * 20.0f + -10.0f, 70.0f), Quaternion.identity);
+            obj.GetComponent<Cloud>().SetCloud(0.01f, 0.8f, 1.2f);
+
+        }
+
+        //後ろ
+        if (Random.value <= 0.001f)
+        {
+            // CubeプレハブをGameObject型で取得
+            GameObject obj = (GameObject)Resources.Load("cloud");
+            // Cubeプレハブを元に、インスタンスを生成、
+            Instantiate(obj, new Vector3(-30.0f, Random.value * 20.0f + -10.0f, 70.0f), Quaternion.identity);
+            obj.GetComponent<Cloud>().SetCloud(0.006f, 0.6f, 0.8f);
+
+        }
+
+
+        Count++;
+    }
+}
