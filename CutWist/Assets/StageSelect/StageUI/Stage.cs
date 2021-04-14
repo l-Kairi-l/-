2f45d8276e117f45609e73c31da5010d976298c7
菊池　海璃 <@LAPTOP-DDSV0RHM>
2021-04-14 19:57:47 +0900

    ステージ選択画面の作成

---
+++ CutWist/Assets/StageSelect/StageUI/Stage.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        Renderer renderer;
+
+        renderer = GetComponent<Renderer>();
+        renderer.material.color = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //     Debug.Log(Input.mousePosition);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // トゲにあたった場合
+        if (other.gameObject.tag == "CoursolPoint")
+        {
+            Renderer renderer;
+
+            renderer = GetComponent<Renderer>();
+
+            renderer.material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
+             Debug.Log(transform.position);
+
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        // トゲにあたった場合
+        if (other.gameObject.tag == "CoursolPoint")
+        {
+            Renderer renderer;
+
+            renderer = GetComponent<Renderer>();
+
+            renderer.material.color = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+        }
+    }
+
+    
+}
---
+++ CutWist/Assets/UI/CursolPoint/CursorPoint.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //x=23
+        //y=15
+        float per_W = 23.0f / 2300;
+        float per_H = 15.0f / 1500;
+
+        Vector3 pos =Input.mousePosition;
+        //座標を真ん中に持ってくる
+        pos.x += -2300;
+        pos.y += -1500;
+        //座標を正規化する
+        pos.x *= per_W;
+        pos.y *= per_H;
+        pos.z = -10.0f;
+
+        transform.position = pos;
+
+       // Debug.Log(transform.position);
+
+        //  transform.position = Input.mousePosition;
+
+
+    }
+}
