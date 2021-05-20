39bf29b9ebe2b780fe4ee237dd0f9bb8b2c18d65
sakamoto <final_banana2@yahoo.co.jp>
2021-05-20 19:24:39 +0900

    ステージ3の統合、オブジェクトをプレハブで一括置換できるエディタの追加

---
+++ CutWist/Assets/Editor/MyWindow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <Summary>
+/// ウィンドウを作成するサンプルです。
+/// </Summary>
+public class MyWindow : EditorWindow
+{
+    /// <Summary>
+    /// ウィンドウを表示します。
+    /// </Summary>
+    [MenuItem("Window/MyWindow")]
+    static void Open()
+    {
+        var window = GetWindow<MyWindow>();
+        window.titleContent = new GUIContent("オリジナルのウィンドウ");
+    }
+}
