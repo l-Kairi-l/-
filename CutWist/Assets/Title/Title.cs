84720dc9d736d543ba69b270e069bd15b02bf2a7
菊池　海璃 <@LAPTOP-DDSV0RHM>
2021-05-25 16:09:00 +0900

    背景のスクロール　　Titleのブロック　ライティングの調整

--- CutWist/Assets/Title/Title.cs
+++ CutWist/Assets/Title/Title.cs
@@ -5,16 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
+    private List<string> PrefabList = new List<string>();
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrefabList.Add("W1_3DBlockNormal");
+        PrefabList.Add("W2_3DBlockNormal");
+        PrefabList.Add("W3_3DBlockNormal");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //一番手前
+        if (Random.value <= 0.005f)
+        {
+            GameObject obj;
+            int Type = (int)Random.Range(0.0f, (float)PrefabList.Count);
+            // プレハブをGameObject型で取得
+                    obj = (GameObject)Resources.Load(PrefabList[Type]);
+            // プレハブを元に、インスタンスを生成、
+            Instantiate(obj, new Vector3(-30.0f, Random.value * 20.0f + -10.0f, 5.0f), Quaternion.identity);
+
+        }
+
+
     }
 
     public void InstanceUI()
